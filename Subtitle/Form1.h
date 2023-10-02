bf11920490a7ca4a20aebcb51d3045aa69860f51
yadesardar14 <m.09391633173@gmail.com>
2023-10-03 01:44:23 +0330

    Add project files.

---
+++ Subtitle/Form1.h
@@ -0,0 +1,77 @@
+#pragma once
+#include "SUB_H.h"
+
+namespace CppCLRWinFormsProject {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for Form1
+	/// </summary>
+	public ref class Form1 : public System::Windows::Forms::Form
+	{
+	public:
+		Form1(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+
+			MessageBox::Show(gcnew String(std::to_string(SET_SUBTI5LE("C:\\Users\\m0939\\Desktop\\morteza_gh\\New folder", "The Rebel S01E03 720p WEB-DL unicode.srt", 5)).c_str()));
+			
+			//subtitle ep("C:\\Users\\m0939\\Desktop\\morteza_gh\\New folder\\The Rebel S01E03 720p WEB-DL unicode.srt",3,0);
+			//if(ep.sub())
+			//MessageBox::Show("ok");
+			//else
+			//	MessageBox::Show("eroor");
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~Form1()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->SuspendLayout();
+			// 
+			// Form1
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(386, 285);
+			this->Name = L"Form1";
+			this->Text = L"Form1";
+			this->Load += gcnew System::EventHandler(this, &Form1::Form1_Load);
+			this->ResumeLayout(false);
+
+		}
+#pragma endregion
+	private: System::Void Form1_Load(System::Object^ sender, System::EventArgs^ e) {
+	}
+	};
+}
